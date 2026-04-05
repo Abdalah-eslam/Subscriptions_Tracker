387952d61079d407e4e33f02cf20589c565a6da7
Abdalah-eslam <abdallaheslam2001@gmail.com>
2026-04-06 00:20:56 +0200

    add rate limiting to ather routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { Register , Login , Signout} from "../controllers/auth.controller.js";
+import arcjetMiddleware from "../middlewares/arcjet.midlleware.js";
 const authRouter = Router();
+authRouter.use(arcjetMiddleware)
 authRouter.post("/login", Login);
 authRouter.post("/register", Register);
 authRouter.post("/signout", Signout);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { getAllUsers ,getUser ,UpdateUser ,DeleteUser} from "../controllers/user.controller.js";
 import {authorize} from "../middlewares/authMidlleware.js";
+import arcjetMiddleware from "../middlewares/arcjet.midlleware.js";
 const userRouter = Router();
+userRouter.use(arcjetMiddleware)
 userRouter.get("/",getAllUsers ),
 userRouter.get("/:id",getUser );
 userRouter.put("/", authorize, UpdateUser);
